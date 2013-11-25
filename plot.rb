fdc562f896ebe6412779ad05184cdc079a00ca86
Joe Zuntz <joezuntz@googlemail.com>
2013-11-25 13:38:57 +0000

    fix plot.rb and make it depend on matplotlib

--- plot.rb
+++ plot.rb
@@ -3,7 +3,8 @@ require 'formula'
 class Plot < Formula
   homepage 'https://github.com/joezuntz/plot'
   head 'https://github.com/joezuntz/plot.git'
-
+  depends_on :python => "matplotlib"
   def install
   	bin.install('plot')
+  end
 end
