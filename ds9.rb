529494c5756957c5982e0f534de9cc661b7298f5
Joe Zuntz <joezuntz@googlemail.com>
2013-11-25 12:54:36 +0000

    added sha1 for ds9 files

--- ds9.rb
+++ ds9.rb
@@ -4,16 +4,16 @@ class Ds9 < Formula
   homepage 'http://hea-www.harvard.edu/RD/ds9/'
   if MacOS.version == :snow_leopard
     url 'http://hea-www.harvard.edu/RD/ds9/download/darwinsnowleopard/ds9.darwinsnowleopard.7.3b5.tar.gz'
-    sha1 ''
+    sha1 'e96d00d18c739407442a09ee931dadbe1dd435ca'
   elsif MacOS.version == :lion
-    url 'http://hea-www.harvard.edu/RD/ds9/download/darwinsnowleopard/ds9.darwinsnowleopard.7.3b5.tar.gz'
-    sha1 ''
+    url 'http://hea-www.harvard.edu/RD/ds9/download/darwinlion/ds9.darwinlion.7.3b5.tar.gz'
+    sha1 '5d7f0448e80298d71d9b0addf717e227c035b297'
   elsif MacOS.version == :mountain_lion
-    url 'http://hea-www.harvard.edu/RD/ds9/download/darwinsnowleopard/ds9.darwinsnowleopard.7.3b5.tar.gz'
-    sha1 ''
+    url 'http://hea-www.harvard.edu/RD/ds9/download/darwinmountainlion/ds9.darwinmountainlion.7.3b5.tar.gz'
+    sha1 '3bb2f7d15c8b6f366e7f19c5408acd47800ac18c'
   elsif MacOS.version == :mavericks
-    url 'http://hea-www.harvard.edu/RD/ds9/download/darwinsnowleopard/ds9.darwinsnowleopard.7.3b5.tar.gz'
-    sha1 ''
+    url 'http://hea-www.harvard.edu/RD/ds9/download/darwinmavericks/ds9.darwinmavericks.7.3b5.tar.gz'
+    sha1 '9443a0317e49eff9d79b7f7493e4429cda591f1e'
   else
     onoe "ds9 is only available for Snow Leopard and above"
   end
