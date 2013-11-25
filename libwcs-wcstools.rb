e5810a10c2d0314638fdf6ebc0ac9b16227175ec
Joe Zuntz <joezuntz@googlemail.com>
2013-11-25 12:32:58 +0000

    added SHA1

--- libwcs-wcstools.rb
+++ libwcs-wcstools.rb
@@ -1,8 +1,9 @@
 require 'formula'
 
-class WCStoolsLibWCS < Formula
+class LibwcsWcstools < Formula
   homepage 'http://tdc-www.harvard.edu/wcstools/'
   url 'http://tdc-www.harvard.edu/software/wcstools/wcstools-3.8.7.tar.gz'
+  sha1 '412d22938b05110eb86f7b01a874c17447ad2f19'
   depends_on 'cfitsio'
 
   def install
